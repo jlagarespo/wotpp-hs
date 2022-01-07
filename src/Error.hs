8bb331a2bf654e213a4093b3729b57ccaacff2ad
Jacob Lagares Pozo <jlagarespo@protonmail.com>
2022-01-07 16:27:40 +0100

    Backtraces.

--- src/Error.hs
+++ src/Error.hs
@@ -1,17 +1,22 @@
 module Error where
 
+import Data.List (intercalate)
 import Data.Text.Lazy (Text, unpack)
 import Text.Parsec
 
 import AST
 
 data EvalError = NotInScope Identifier Int | MatchFail Expr Int
-data Error = EvalErr EvalError | ParseErr ParseError
+data Error = EvalErr EvalError [Expr] | ParseErr ParseError
 
 instance Show EvalError where
   show (NotInScope id args) = "Applying " ++ show args ++ " arguments to function " ++ unpack id ++ " not in scope."
   show (MatchFail what n) = "Attempted to match " ++ show what ++ " against " ++ show n ++ " patterns. Please ensure all your matches are exhaustive."
 
 instance Show Error where
-  show (EvalErr err)  = "eval: " ++ show err
-  show (ParseErr err) = "parse: " ++show err
+  show (EvalErr err trace) =
+    (if null trace
+     then "eval error:\n"
+     else "while evaluating " ++ intercalate "\n in " (map show trace) ++ ":\n")
+    ++ show err
+  show (ParseErr err) = "parse error:\n" ++show err
--- src/Eval.hs
+++ src/Eval.hs
@@ -15,40 +15,49 @@ import Error
 import Util (onlyRights)
 
 type FuncId = (Identifier, Int)
-newtype Env = Env (HashMap (Identifier, Int) Function)
 data Function = Function [Identifier] Body deriving Show
+data Env = Env
+  { functions :: HashMap FuncId Function
+  , trace :: [Expr] }
+
+evalError :: Env -> EvalError -> Either Error a
+evalError (Env _ trace) err = Left $ EvalErr err trace
 
 newenv :: Env
-newenv = Env HM.empty
+newenv = Env HM.empty []
 
 evalExpr :: Env -> Expr -> Either Error Text
 evalExpr _ (ELit str) = pure str
-evalExpr (Env functions) (EApp id args) = do
+evalExpr (Env functions trace) e@(EApp id args) = do
+  let env = Env functions (e:trace)
+
   -- Retrieve function from environment.
   (Function params body) <-
     case functions !? (id, length args) of
-      Nothing -> Left $ EvalErr $ NotInScope id (length args)
+      Nothing -> evalError env $ NotInScope id (length args)
       Just f  -> pure f
 
   -- Add arguments to the environment as unevaluated functions.
   -- TODO: Parameter shadowing warnings.
   let newfuncs = HM.fromList $ zipWith (\param arg -> ((param, 0), Function [] (Body [] arg))) params args
-      env' = Env (functions `HM.union` newfuncs)
+      env' = Env (functions `HM.union` newfuncs) (e:trace)
 
   -- Evaluate all the statements to acquire our final environment.
   evalBody env' body
 
-evalExpr env (ECat l r) = do
+evalExpr (Env fs trace) e@(ECat l r) = do
+  let env = Env fs (e:trace)
   l' <- evalExpr env l
   r' <- evalExpr env r
   pure $ l' <> r'
 
-evalExpr env (EMatch what branches) = do
+evalExpr (Env fs trace) e@(EMatch what branches) = do
+  let env = Env fs (e:trace)
   what' <- evalExpr env what
   branches' <- onlyRights $ map (\(l, r) -> (, r) <$> evalExpr env l) branches
   case find (\(l, r) -> l == what') branches' of
     Just (l, r) -> evalBody env r
-    Nothing -> Left $ EvalErr $ MatchFail what (length branches)
+    Nothing -> evalError env $ MatchFail what (length branches)
 
 evalBody :: Env -> Body -> Either Error Text
 evalBody env (Body statements expr) = do
@@ -57,15 +66,15 @@ evalBody env (Body statements expr) = do
 
 
 evalStatements :: Env -> [Statement] -> Either Error (Text, Env)
-evalStatements env ((SExpr expr):ss) = do
+evalStatements env@(Env fs _) ((SExpr expr):ss) = do
   (rest, env') <- evalStatements env ss
-  e <- evalExpr env expr
+  e <- evalExpr (Env fs []) expr
   pure (e <> rest, env')
 
-evalStatements (Env functions) ((SFunction id params body):ss) =
+evalStatements (Env functions _) ((SFunction id params body):ss) =
   -- TODO: Function shadowing warnings.
   let func = Function params body
-      newenv = Env (HM.insert (id, length params) func functions)
+      newenv = Env (HM.insert (id, length params) func functions) []
   in evalStatements newenv ss
 
 evalStatements env [] = pure ("", env)
